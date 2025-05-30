dca9340b78ae520c7aa1b8c769c831718e40936e
treslecheson <tr3ysonb@gmail.com>
2025-05-30 10:58:23 -0600

    solution

--- is-anagram-go/anagram.go
+++ is-anagram-go/anagram.go
@@ -1,5 +1,24 @@
 package main
 
+import "strings"
+
 func isAnagram(str1, str2 string) bool {
+	if len(str1) != len(str2) {
+		return false
+	}
+	for i := 0; i < len(str1); i++ {
+		if strings.Contains(strings.ToLower(str2), strings.ToLower(string(str1[i]))) {
+			strings.Replace(str2, string(str1[i]), "", 1)
+		} else {
+			return false
+		}
+	}
 	return true
+
+}
+
+func main() {
+	println(isAnagram("Sled", "LEDs"))
+	println(isAnagram("read", "dear"))
+	println(isAnagram("deer", "deere"))
 }
---
+++ is-anagram-go/go.mod
@@ -0,0 +1,3 @@
+module is-anagram
+
+go 1.24.2
